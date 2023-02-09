473386dec6d3392ddfda8c5106ae6d9afb68fcb5
Emanuela91 <emanuela.marson91@gmail.com>
2023-02-09 17:40:57 +0100

    stampati in home tutti i progetti

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,7 +4,17 @@
     <h1>Progetti</h1>
     <ul>
         @foreach ($projects as $project)
-            <li>{{ $project -> name }}</li>
+            <li>{{$project -> main_image}}
+                <br>
+                {{$project -> name}}
+                <br>
+                Descrizione: {{$project -> description}}
+                <br>
+                Data Pubbilcazione: {{$project -> release_date}}
+                <br>
+                Link Github: {{$project -> repo_link}}
+                <br>--------------------- <br>
+            </li>
         @endforeach
     </ul>
 @endsection
