3e8b972a6808fa4a0f4ea57c28975f0fc12c7598
Emanuela91 <emanuela.marson91@gmail.com>
2023-02-10 12:22:06 +0100

    aggiunto @auth-@authend in home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,9 +2,12 @@
 
 @section('content')
     <h1>Progetti</h1>
+    {{-- con @auth @endauth mostro ciò che c'è all'interno solo quando sono loggata --}}
+    @auth
     <a href="{{route('admin.project.store')}}">
     Crea un nuovo progetto
-    </a>
+    </a>        
+    @endauth
     <ul>
         @foreach ($projects as $project)
             <li><img src="{{$project -> main_image}}" alt="">
